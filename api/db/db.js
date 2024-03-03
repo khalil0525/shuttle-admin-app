9913f0f70465b19e3223aaae7a85ab8b334bada9
Khalil Collins <Collinskhalil@hotmail.com>
2024-03-03 10:14:05 -0500

    update

--- api/db/db.js
+++ api/db/db.js
@@ -1,29 +1,24 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 const db =
-  process.env.env === 'development'
+  process.env.env === "development"
     ? new Sequelize({
         database: process.env.DB_DATABASE,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: 'postgres',
+        dialect: "postgres",
         logging: false, // Turning off logging
       })
-    : new Sequelize({
-        database: process.env.DB_DATABASE,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-        logging: false, // Turning off logging
+    : new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+
         dialectOptions: {
           ssl: {
             require: false,
-            rejectUnauthorized: false, // You might need to set this to true in production
+            rejectUnauthorized: false,
           },
         },
       });
